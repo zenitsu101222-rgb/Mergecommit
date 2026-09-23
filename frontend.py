f9491904d16c0f82d3aebb471ba9bbf67ba1f27c
Zenitsu <zenitsu101222@gmail.com>
2026-09-23 15:01:43 +0530

    frontend.py

--- frontend.py
+++ frontend.py
@@ -2,4 +2,7 @@ this the dummy file to test the git tool in the ubuntu system.
 
 and this the learning file to push the file in git o github
 
+line added.
+
+this line to check dev A push code. and dev B want to update code in his repo.
 
